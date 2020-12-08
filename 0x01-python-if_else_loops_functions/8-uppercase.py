2e7d830789882e96637bdb5c5a9cb142179aa1af
sang-nguy0920 <2321@holbertonschool.com>
2020-12-08 21:13:45 +0000

    Added file to 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        if ord(char) in range(97, 123):
+            result += chr(ord(char) - 32)
+        else:
+            result += chr(ord(char))
+    print(result)
