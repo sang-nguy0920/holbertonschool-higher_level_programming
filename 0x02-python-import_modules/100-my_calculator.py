c4d707e65cc37d6948d71422ad83398b2641b759
sang-nguy0920 <2321@holbertonschool.com>
2020-12-11 16:46:34 +0000

    Added file 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,18 +6,18 @@ if __name__ == "__main__":
     x = sys.argv
     if len(x) != 4:
         (print("{}"
-        .format("Usage: ./100-my_calculator.py <a> <operator> <b>")))
+         .format("Usage: ./100-my_calculator.py <a> <operator> <b>")))
         exit(1)
     if not x[2] in operators:
         (print("{}"
-        .format("Unknown operator. Available operators: +, -, * and /")))
+         .format("Unknown operator. Available operators: +, -, * and /")))
         exit(1)
     a = int(x[1])
     b = int(x[3])
     op = x[2]
     result = (calc.add(a, b)
-                if op is "+" else calc.sub(a, b)
-                if op is "-" else calc.mul(a, b)
-                if op is "*" else calc.div(a, b)
-                if op is "/" else 0)
+              if op is "+" else calc.sub(a, b)
+              if op is "-" else calc.mul(a, b)
+              if op is "*" else calc.div(a, b)
+              if op is "/" else 0)
     print("{} {} {} = {}".format(a, op, b, result))
