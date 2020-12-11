961189f840bd57e22531da2990e3950f3eb8b6a5
sang-nguy0920 <2321@holbertonschool.com>
2020-12-11 00:38:24 +0000

    Fixed code in file 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 1:
-        print("{} arguments.".format(len(sys.argv) - 1))
-    elif len(sys.argv) == 2:
-        print("{} argument:".format(len(sys.argv) - 1))
+    x = sys.argv
+    if len(x) == 1:
+        print("{} arguments.".format(len(x) - 1))
+    elif len(x) == 2:
+        print("{} argument:".format(len(x) - 1))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-    for args in range(len(sys.argv)):
+        print("{} arguments:".format(len(x) - 1))
+    for args in range(len(x)):
         if args == 0:
             continue
-        print("{:d}: {:s}".format(args, sys.argv[args]))
+        print("{:d}: {:s}".format(args, x[args]))
