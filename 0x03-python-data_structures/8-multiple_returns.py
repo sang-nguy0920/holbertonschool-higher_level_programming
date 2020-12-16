533d93a4b4cb92e5559faade5341bda968c231d9
sang-nguy0920 <2321@holbertonschool.com>
2020-12-16 16:37:12 +0000

    Fixed 8-multiple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    sentence_length = len(sentence)
-    first_character = ""
-    if sentence_length == 0:
-        first_character = None
-    else:
-        first_character = sentence[0]
-    return (sentence_length, first_character)
+    if not sentence:
+        return 0, None
+    return (len(sentence), sentence[0])
