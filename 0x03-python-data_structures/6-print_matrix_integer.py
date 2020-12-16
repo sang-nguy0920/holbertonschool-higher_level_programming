f37c1e8dafe0c89cc142c6b7513b857dbaf9c61c
sang-nguy0920 <2321@holbertonschool.com>
2020-12-16 02:50:43 +0000

    Added files for 6-print.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        print("{}".format(matrix[0]))
+        for row in range(len(matrix)):
+            for element in range (len(matrix[row])):
+                if element != len(matrix[row]) - 1:
+                    punc = " "
+                else:
+                    punc = ""
+                print("{:d}".format(matrix[row][element]), end=punc)
+            print()
