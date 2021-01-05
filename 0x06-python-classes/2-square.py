7fe71a7fc9a18b3679a0e9d18e7345fb54995f88
sang-nguy0920 <2321@holbertonschool.com>
2021-01-05 23:32:52 +0000

    Added files for 2. Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class Square that defines a square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """Define size of the square"""
+        if type(size) == int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
