b58af9813272cb3df80fafcae4e2cc0edf52f385
sang-nguy0920 <2321@holbertonschool.com>
2021-01-05 23:26:49 +0000

    Added files for 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class Square that defines a square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """Define size of the square"""
+        self.__size = size
