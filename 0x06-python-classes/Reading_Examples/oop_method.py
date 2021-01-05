2f14ad74e7eba6b03e1b421f351c8a99bb8f3582
sang-nguy0920 <2321@holbertonschool.com>
2021-01-05 22:04:42 +0000

    Created dir Reading_Examples and files

---
+++ 0x06-python-classes/Reading_Examples/oop_method.py
@@ -0,0 +1,7 @@
+# simple example of classes/objects methods
+class Person:
+    def say_hi(self):
+        print('Hello!')
+p = Person()
+p.say_hi()
+# can also be written as Person().say_hi()
---
+++ 0x06-python-classes/Reading_Examples/oop_simplestclass.py
@@ -0,0 +1,5 @@
+# simple example of classes
+class Person:
+    pass
+p = Person()
+print(p)
