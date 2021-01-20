734f4f74b7c11dc00c2381d0885d4439f81e2cfd
sang-nguy0920 <2321@holbertonschool.com>
2021-01-20 00:58:37 +0000

    Added files for 6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry"""
+
+
+class BaseGeometry:
+    """geometry base class"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        """implements area"""
+        raise Exception("area() is not implemented")
