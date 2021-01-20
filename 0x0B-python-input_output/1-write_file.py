861af69cc55ec4cd64eb717bbd2af84c0ee867ee
sang-nguy0920 <2321@holbertonschool.com>
2021-01-20 22:57:20 +0000

    Added files for 1. Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    """writes a string to a text file, returns the number of characters written"""
+    num = 0
+    with open(filename, mode='w', encoding='utf-8') as f:
+        num += f.write(text)
+        f.close()
+    return num
