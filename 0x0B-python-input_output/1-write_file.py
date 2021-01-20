c4b9e4451c18f161235872d67863b26bb3154247
sang-nguy0920 <2321@holbertonschool.com>
2021-01-20 23:11:55 +0000

    Added files for 1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """writes a string to a text file, returns the number of characters written"""
+    """writes a string to a text file, returns the num written"""
     num = 0
     with open(filename, mode='w', encoding='utf-8') as f:
         num += f.write(text)
