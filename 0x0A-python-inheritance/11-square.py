bd2ddb876dbcca76a98c9e3706017d9e9a3dbb8a
sang-nguy0920 <2321@holbertonschool.com>
2021-01-20 04:32:20 +0000

    Added files for 10. Square #1

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+    def __init__(self, size):
+        """inits object"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+        self.area()
+
+    def __str__(self):
+        """prints square description"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
