5914213b8ae326ad76d2f13815ca1c2bd2496e1b
sang-nguy0920 <2321@holbertonschool.com>
2021-01-25 23:47:55 +0000

    Updated files for 8. Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,3 +90,17 @@ class Rectangle(Base):
         """returns string rep of rectangle"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args):
+        """updates/assigns an arg to each attribute"""
+        for i in range(len(args)):
+            if i == 0:
+                self.id = args[0]
+            if i == 1:
+                self.width = args[1]
+            if i == 2:
+                self.height = args[2]
+            if i == 3:
+                self.x = args[3]
+            if i == 4:
+                self.y = args[4]
