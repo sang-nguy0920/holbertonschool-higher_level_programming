e3b3a8b26f06cd6d9d519665a3776003fa52ed86
sang-nguy0920 <2321@holbertonschool.com>
2021-03-03 04:08:37 +0000

    Updated file 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,5 @@
 -- Script that lists all shows, and all genres linked to that show.
+
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
