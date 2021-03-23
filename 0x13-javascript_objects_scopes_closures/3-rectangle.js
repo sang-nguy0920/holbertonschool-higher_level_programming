95b8490e7c67dea92a2fb0ab200a9642b5e324d3
sang-nguy0920 <2321@holbertonschool.com>
2021-03-23 17:43:19 +0000

    Added files for 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      for (let y = 0; y < this.width; y++) {
+        process.stdout.write('X');
+      }
+      console.log();
+    }
+  }
+};
