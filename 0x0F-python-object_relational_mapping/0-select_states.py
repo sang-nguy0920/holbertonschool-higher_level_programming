1a938f41e20387d085eb2a9ce58f8601fde3c1e4
sang-nguy0920 <2321@holbertonschool.com>
2021-03-25 23:51:33 +0000

    Added files for task 0.

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module with a script that lists all states from the database hbtn_0e_0_usa.
+"""
+
+
+import MySQLdb
+import sys
+
+
+def goto_db():
+    """Inits a database"""
+    database = MySQLdb.connect(host="localhost", port=3306,
+                               user=sys.argv[1], passwd=sys.argv[2],
+                               db=sys.argv[3])
+    return database
+
+
+def list_states(database):
+    """lists all states in database"""
+    cur = database.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    get_rows = cur.fetchall()
+    for row in get_rows:
+        print(row)
+    cur.close()
+    database.close()
+
+if __name__ == "__main__":
+    list_states(goto_db())
