55477bd94634963d1986ff0a7d00c8320382c95d
sang-nguy0920 <2321@holbertonschool.com>
2021-03-26 20:49:30 +0000

    Added files for task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,8 +7,24 @@ Start link class to table in database
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
+def session_st():
+    """fill in later for checker"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return (engine, session)
+
+def list_states(database):
+    """lists all states in database"""
+    session = database[1]
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, ': ', instance.name, sep='')
+
+    session.close()
+    database[0].dispose()
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    list_states(session_st())
