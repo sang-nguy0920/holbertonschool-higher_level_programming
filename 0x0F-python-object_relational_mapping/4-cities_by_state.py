4b8794e3d61de0e8e9e74569e31d78172569b9c8
sang-nguy0920 <2321@holbertonschool.com>
2021-03-26 18:01:49 +0000

    Added files for task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module with a script that lists all states from the database hbtn_0e_0_usa.
+"""
+
+
+import MySQLdb
+import sys
+
+
+def goto_db():
+    """Inits a database"""
+    database = MySQLdb.connect(host="localhost", port=3306,
+                               user=sys.argv[1], passwd=sys.argv[2],
+                               db=sys.argv[3])
+    return database
+
+
+def list_cities(database):
+    """lists all states in database"""
+    cur = database.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name \
+                 FROM cities \
+                 INNER JOIN states ON states.id = cities.state_id \
+                 ORDER BY cities.id ASC")
+    get_rows = cur.fetchall()
+    for row in get_rows:
+        print(row)
+    cur.close()
+    database.close()
+
+if __name__ == "__main__":
+    list_cities(goto_db())
