ff66879f3e72810a8fe8c5d7dc3d4d642732cd9c
sang-nguy0920 <2321@holbertonschool.com>
2021-03-29 02:53:13 +0000

    Added files for task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Start link class to table in database
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def session_st():
+    """inits session"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return (engine, session)
+
+
+def add_state_obj(database):
+    """prints state to database"""
+    session = database[1]
+    state_obj = State()
+    state_obj.name = "Louisiana"
+    session.add(state_obj)
+    session.commit()
+    instance_a = session.query(State).filter(State.name == "Louisiana")
+    print(instance_a[0].id)
+
+    session.close()
+    database[0].dispose()
+
+if __name__ == "__main__":
+    add_state_obj(session_st())
