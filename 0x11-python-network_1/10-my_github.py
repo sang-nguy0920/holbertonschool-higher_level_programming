551328ced4a652a92b20fdf52d5c54be4c91eeaf
sang-nguy0920 <2321@holbertonschool.com>
2021-04-10 01:54:05 +0000

    worth a shot

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    from requests.auth import HTTPBasicAuth
+    from sys import argv
+    fetch = requests.get('https://api.github.com/users/{}'.format(argv[1]),
+                         auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(fetch.json().get('id'))
