714833e2c3968843a66f7030fb9d0b33ae41d3e8
sang-nguy0920 <2321@holbertonschool.com>
2021-05-11 15:20:12 +0000

    added file for task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const host = process.argv[2];
+
+request(host, function (err, res, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  let occ = 0;
+  for (const film of JSON.parse(body).results) {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        occ = occ + 1;
+      }
+    }
+  }
+  console.log(occ);
+});
